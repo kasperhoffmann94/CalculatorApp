104c5250a7f5a7b6d739b15b996ba06dc8fec552
LAPTOP-4LGA03E1\kaspe <kasperhoffmann94@gmail.com>
2018-10-09 12:46:54 +0200

    Add project files.

---
+++ CalculatorApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SmartMenuLibrary;
+using CalculatorLibrary;
+
+namespace CalculatorApp.cs
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Program myProgram = new Program();
+            myProgram.Run();
+        }
+
+        private void Run()
+        {
+
+            Bindings bindings = new Bindings();
+            SmartMenu menu = new SmartMenu();
+            menu.LoadMenu(":MenuSpec.txt");
+            menu.Activate(bindings);
+        }
+    }
+}
